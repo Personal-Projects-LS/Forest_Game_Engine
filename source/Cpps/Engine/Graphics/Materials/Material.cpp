3df2cfde84d9a332ce863abefe69d9a3657480af
LincolnSand <sandlincoln@gmail.com>
2020-04-10 23:50:02 -0700

    switched from PNG to BMP

--- source/Cpps/Engine/Graphics/Materials/Material.cpp
+++ source/Cpps/Engine/Graphics/Materials/Material.cpp
@@ -2,7 +2,7 @@
 #include "Headers/Engine/Graphics/Materials/Material.h"
 
 Material::Material(const char* directory, int type) {
-    std::string extension = type == Texture::PNG ? "png" : "jpg";
+    std::string extension = type == Texture::PNG ? "bmp" : "jpg";
     this -> albedo = Texture(((std::string(directory) + std::string("/albedo."))+ extension).c_str(), 0, "albedo");
     this -> ao = Texture(((std::string(directory) + std::string("/ao."))+ extension).c_str() , 1, "ao");
     this -> metallic = Texture(((std::string(directory) + std::string("/metallic."))+ extension).c_str() , 2, "metallic");
@@ -31,4 +31,3 @@ Texture &Material::getNormal() {
 Texture &Material::getRoughness() {
     return roughness;
 }
-
